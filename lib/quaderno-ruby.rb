27e8747a0481f68fba016448cd7a2eac17846487
Sebastien Auriault <sebastienauriault@gmail.com>
2021-12-07 16:25:34 -0500

    Add Transaction object

--- lib/quaderno-ruby.rb
+++ lib/quaderno-ruby.rb
@@ -10,4 +10,4 @@ require 'quaderno-ruby/helpers/authentication'
 require 'quaderno-ruby/collection'
 
 %w(block crud deliver payment retrieve).each { |filename| require "quaderno-ruby/behavior/#{filename}" }
-%w(base contact item invoice receipt credit income estimate expense recurring document_item report report_request evidence payment webhook tax checkout_session account tax_id tax_rate jurisdiction).each { |filename| require "quaderno-ruby/#{ filename }" }
+%w(base contact item invoice receipt credit income estimate expense recurring document_item report report_request evidence payment webhook tax checkout_session account tax_id tax_rate jurisdiction transaction).each { |filename| require "quaderno-ruby/#{ filename }" }
---
+++ lib/quaderno-ruby/transaction.rb
@@ -0,0 +1,4 @@
+class Quaderno::Transaction < Quaderno::Base
+  api_model Quaderno::Transaction
+  api_path 'transactions'
+end
